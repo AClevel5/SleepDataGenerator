943a240ae207cf620ba5e0857bf1bb913d4a1a07
alexcleveland <alexcleveland5@gmail.com>
2026-01-12 16:59:39 -0700

    Added some randomness to the times

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 # Sleep Format 2026-1-01 020:51:00;2026-1-02 07:51:00
 # This imports in UTC so to get to MTN we have to shift times 7 hours forward therefore day 1 and 2 start on the subsequent days
 # 5:00 UTC is really 22:00 MTN and 13:00 UTC is really 06:00
@@ -7,12 +8,18 @@ year = int(input("What year would you like? 1, 2, 3"))
 day2 = day1 + 1
 day1text = ""
 day2text = ""
-# Set this to days in the month
 days_per_month = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 days_in_month = days_per_month[month - 1]
 
 
 for x in range(0,days_in_month - day1):
+    rand_hour1 = random.randint(3, 5)
+    rand_hour2 = random.randint(13, 16)
+    rand_minute1 = random.randint(10, 59)
+    rand_minute2 = random.randint(10, 59)
+    rand_second1 = random.randint(10, 59)
+    rand_second2 = random.randint(10, 59)
+    rand_hour1_text = "0" + str(rand_hour1)
     if day1 < 10:
         day1text = "0" + str(day1)
     else:
@@ -21,6 +28,6 @@ for x in range(0,days_in_month - day1):
         day2text = "0" + str(day2)
     else:
         day2text = str(day2)
-    print(f"{year}-{month}-{day1text} 05:00:00;{year}-{month}-{day2text} 13:00:00")
+    print(f"{year}-{month}-{day1text} {rand_hour1_text}:{rand_minute1}:{rand_second1};{year}-{month}-{day2text} {rand_hour2}:{rand_minute2}:{rand_second2}")
     day1 += 1
     day2 += 1
